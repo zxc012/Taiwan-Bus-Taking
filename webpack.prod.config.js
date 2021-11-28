74cc3f5648fa5d3963094d96c3a01457b2c145fd
Howard <rt1593571@gmail.com>
2021-11-29 00:37:03 +0800

    [修改]webpack的publicPath為/

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         filename: './scripts/[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/Taiwan-Bus-Taking/',
+        publicPath: '/',
         clean: true
     },
     // devServer: {
